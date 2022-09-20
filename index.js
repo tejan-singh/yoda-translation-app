312dcc490d6c277d78940f54375917ec007d11a3
Tejan Singh <tstejan@gmail.com>
2022-09-20 19:31:24 +0530

    completed yoda speak app

---
+++ index.js
@@ -0,0 +1,24 @@
+var inputTextBox = document.body.querySelector("#inputTextBox");
+var outputTextBox = document.body.querySelector("#outputTextBox");
+var translateButton = document.body.querySelector("#translate-btn");
+
+var apiUrl = "https://api.funtranslations.com/translate/yoda.json";
+
+const translate = () => {
+  //input
+  var inputText = inputTextBox.value;
+  var requestUrl = apiUrl + "?text=" + inputText;
+
+  //processing
+  fetch(requestUrl)
+    .then((response) => {
+      return response.json();
+    })
+    .then((data) => {
+      console.log(data);
+      //output
+      outputTextBox.value = data.contents.translated;
+    });
+};
+
+translateButton.addEventListener("click", translate);
